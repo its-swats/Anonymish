1228a48437a0e3393b9400007a38622de72a4b53
Shawn Watson <shawnw86@gmail.com>
2016-01-16 21:30:42 -0800

    Update UI to autosize chat box

--- public/javascript/custom.js
+++ public/javascript/custom.js
@@ -1,1 +1,11 @@
-messages = ["message one", "message two", "message three", "message four"]
+messages = ["message one", "message two", "message three", "message four", "message one", "message two", "message three", "message four", "message one", "message two", "message three", "message four", "message one", "message two", "message three", "message four", "end"]
+
+$(function(){
+})
+
+// var setBoxHeight = function() {
+//   var fullsize = $(window).height();
+//   var header = $('.header').height();
+//   var footer = $('.footer').height();
+//   $('.chatMessages').height(fullsize - header - footer)
+// }
