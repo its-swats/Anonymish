fd987eaea17585cd67d6a98cd33ae47418a53de4
Shawn Watson <shawnw86@gmail.com>
2016-01-17 17:24:36 -0800

    More UI work

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,13 @@
 class PostsController < ApplicationController
+  skip_before_action :verify_authenticity_token, only: [:create]
+
+  def index
+    @posts = Post.all
+    render json: @posts
+  end
+
+  def create
+    Post.create(content: params[:post])
+    render json: { :status => :ok }
+  end
 end
