e7b9b3abe3748e91eb3a91ef2a6bab26fd249371
Shawn Watson <shawnw86@gmail.com>
2016-01-17 17:41:54 -0800

    Adding and receiving posts has been AJAXd

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   skip_before_action :verify_authenticity_token, only: [:create]
 
   def index
-    @posts = Post.all
+    @posts = Post.all.order(id: :desc).limit(8)
     render json: @posts
   end
 
