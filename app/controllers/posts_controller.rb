4859cdb9ccf865e6ee59eeafe72f52849f783bc8
Shawn Watson <shawnw86@gmail.com>
2016-01-18 16:22:50 -0800

    Add timestamps to messages

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   skip_before_action :verify_authenticity_token, only: [:create]
 
   def index
-    @posts = Post.all.order(id: :desc).limit(8)
+    @posts = Post.all.order(id: :desc).limit(20)
     render json: @posts
   end
 
